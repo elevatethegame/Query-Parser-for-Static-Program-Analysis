34c5bcc7799cff8084ff3119a98c6b6b63b83784
zhaohuanqdcn <zhaohuanqdcn@gmail.com>
2021-02-07 23:16:58 +0800

    Update PKB header

--- Team00/Code00/src/spa/src/PKB.cpp
+++ Team00/Code00/src/spa/src/PKB.cpp
@@ -1,17 +1,58 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
-#include <vector>
+#include <set>
+#include <map>
+
+#include "EntityType.h"
 
 using namespace std;
 
 #include "PKB.h"
-#include "TNode.h"
 
-int PKB::setProcToAST(PROC p, TNode* r) {
-	return 0;
+PKB::PKB(int n) : number(n), types(n) {}
+
+void PKB::init() {
 }
 
-TNode* PKB::getRootAST (PROC p){
-	return nullptr;
+bool PKB::setStatementType(int index, EntityType type) {
+	return true;
+}
+
+bool PKB::insertParent(int parent, int child) {
+	return true;
+}
+
+
+bool PKB::insertFollow(int former, int latter) {
+	return true;
+}
+
+
+bool PKB::insertDirectUses(int index, set<string> variables) {
+	return true;
+}
+
+bool PKB::insertDirectModifies(int index, string variables) {
+		return true;
+}
+
+bool PKB::insertExpression(int index, long expression) {
+	return true;
+}
+
+set<string> PKB::getEntities(EntityType type) {
+	return set<string>();
+}
+
+void PKB::extractFollowStar() {
+}
+
+void PKB::extractParentStar() {
+}
+
+void PKB::extractUses() {
+}
+
+void PKB::extractModifies() {
 }
