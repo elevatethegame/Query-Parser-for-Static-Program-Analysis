2844cf2f27ccfee44f9f2bacfc72fec2fce2a48b
zhaohuanqdcn <zhaohuanqdcn@gmail.com>
2021-02-08 12:39:50 +0800

    Update set & get of stmt types

--- Team00/Code00/src/spa/src/PKB.cpp
+++ Team00/Code00/src/spa/src/PKB.cpp
@@ -2,47 +2,73 @@
 #include <iostream>
 #include <string>
 #include <set>
-#include <map>
+#include <unordered_map>
 
+#include "PKB.h"
 #include "EntityType.h"
+#include "RelationshipType.h"
 
 using namespace std;
 
-#include "PKB.h"
-
-PKB::PKB(int n) : number(n), types(n) {}
+PKB::PKB(const int& n) : number(n) {}
 
 void PKB::init() {
 }
 
-bool PKB::setStatementType(int index, EntityType type) {
+bool PKB::setStatementType(const int& index, const EntityType& type) {
+	if (type == STMT || type == VAR || type == CONST || type == PROC) {
+		return false;
+	} else if (index > this->number || index <= 0) {
+		return false;
+	} else {
+		string indexString = to_string(index);
+		if (this->types.find(indexString) != types.end()) { // key exists
+			return false;
+		} else {
+			this->types[indexString] = type;
+			this->stmts[type].insert(indexString);
+			return true;
+		}
+	}
+}
+
+bool PKB::insertParent(const int& parent, const int& child) {
+	if (parent <= 0 || parent > this->number) {
+		return false;
+	} else if (child <= 0 || child > this->number) {
+		return false;
+	} else {
+		int t = RelationshipType::FOLLOWS;
+		this->relations[t][to_string(parent)].insert(to_string(child));
+		this->relationsBy[t][to_string(child)].insert(to_string(parent));
+		return true;
+	}
+}
+
+
+bool PKB::insertFollow(const int& former, const int& latter) {
 	return true;
 }
 
-bool PKB::insertParent(int parent, int child) {
+
+bool PKB::insertDirectUses(const int& index, const set<string>& variables) {
 	return true;
 }
 
-
-bool PKB::insertFollow(int former, int latter) {
-	return true;
-}
-
-
-bool PKB::insertDirectUses(int index, set<string> variables) {
-	return true;
-}
-
-bool PKB::insertDirectModifies(int index, string variables) {
+bool PKB::insertDirectModifies(const int& index, const string& variables) {
 		return true;
 }
 
-bool PKB::insertExpression(int index, long expression) {
+bool PKB::insertExpression(const int& index, const long& expression) {
 	return true;
 }
 
-set<string> PKB::getEntities(EntityType type) {
-	return set<string>();
+set<string> PKB::getEntities(const EntityType& type) {
+	if (type == STMT || type == VAR || type == CONST || type == PROC) {
+		return set<string>();
+	} else {
+		return this->stmts[type];
+	}
 }
 
 void PKB::extractFollowStar() {
