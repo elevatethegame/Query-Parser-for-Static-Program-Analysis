cec776e20a2e8a36b30b7f574cc71c42bf5f7e62
zhaohuanqdcn <zhaohuanqdcn@gmail.com>
2021-02-14 15:11:15 +0800

    Finish extract methods

--- Team00/Code00/src/unit_testing/src/TestPKB.cpp
+++ Team00/Code00/src/unit_testing/src/TestPKB.cpp
@@ -183,6 +183,20 @@ TEST_CASE("PKB extarctStar 2") {
 	REQUIRE(pkb.insertParent(2, 15));
 	REQUIRE(pkb.insertFollow(14, 15));
 
+	REQUIRE(pkb.insertDirectUses(2, set<string>{ "x" }));
+	REQUIRE(pkb.insertDirectUses(3, set<string>{ "x", "y" }));
+	REQUIRE(pkb.insertDirectModifies(3, "x"));
+	REQUIRE(pkb.insertDirectUses(5, set<string>{ "a", "b" }));
+	REQUIRE(pkb.insertDirectModifies(6, "y"));
+	REQUIRE(pkb.insertDirectModifies(7, "z"));
+	REQUIRE(pkb.insertDirectUses(9, set<string> { "a", "c", "z" }));
+	REQUIRE(pkb.insertDirectModifies(9, "t"));
+	REQUIRE(pkb.insertDirectUses(12, set<string> { "z", "t" }));
+	REQUIRE(pkb.insertDirectModifies(14, "count"));
+	REQUIRE(pkb.insertDirectUses(15, set<string> { "a" }));
+	REQUIRE(pkb.insertDirectModifies(15, "y"));
+	REQUIRE(pkb.insertDirectUses(16, set<string>{ "m" }));
+
 	pkb.init();
 
 	unordered_map<string, set<string>> resultFollow = pkb.getResultsOfRS(
@@ -225,4 +239,41 @@ TEST_CASE("PKB extarctStar 2") {
 	REQUIRE(set<string>{} == resultParent["1"]);
 	REQUIRE(set<string>{} == resultParent["6"]);
 	REQUIRE(set<string>{} == resultParent["13"]);
+
+	REQUIRE(pkb.getBooleanResultOfRS(MODIFIES, *(new StmtNum(3)), *(new Indent("x"))));
+	REQUIRE(pkb.getBooleanResultOfRS(MODIFIES, *(new StmtNum(5)), *(new Indent("t"))));
+	REQUIRE(pkb.getBooleanResultOfRS(USES, *(new StmtNum(8)), *(new Indent("c"))));
+	REQUIRE(pkb.getBooleanResultOfRS(USES, *(new StmtNum(11)), *(new Any())));
+	REQUIRE(pkb.getBooleanResultOfRS(USES, *(new Any()), *(new Indent("z"))));
+	
+	set<string> resultUses1 = pkb.getResultsOfRS(USES,
+		*(new Declaration(EntityType::STMT, "s")), *(new Indent("t")))[""];
+	string c1[] = { "2", "11", "12" };
+	REQUIRE(set<string>{ std::begin(c1), std::end(c1) } == resultUses1);
+	
+	set<string> resultUses2 = pkb.getResultsOfRS(USES,
+		*(new Declaration(EntityType::STMT, "s")), *(new Indent("z")))[""];
+	string c2[] = { "2", "5", "8", "9", "11", "12" };
+	REQUIRE(set<string>{ std::begin(c2), std::end(c2) } == resultUses2);
+	
+	set<string> resultUses3 = pkb.getResultsOfRS(USES,
+		*(new StmtNum(2)), *(new Declaration(EntityType::VAR, "v")))[""];
+	string c3[] = { "a", "b", "c", "x", "y", "z", "t" };
+	REQUIRE(set<string>{ std::begin(c3), std::end(c3) } == resultUses3);
+
+	set<string> resultModifies1 = pkb.getResultsOfRS(MODIFIES,
+		*(new Declaration(EntityType::STMT, "s")), *(new Indent("t")))[""];
+	string d1[] = { "2", "5", "8", "9" };
+	REQUIRE(set<string>{ std::begin(d1), std::end(d1) } == resultModifies1);
+
+	set<string> resultModifies2 = pkb.getResultsOfRS(MODIFIES,
+		*(new Declaration(EntityType::STMT, "s")), *(new Indent("y")))[""];
+	string d2[] = { "2", "5", "6", "15" };
+	REQUIRE(set<string>{ std::begin(d2), std::end(d2) } == resultModifies2);
+
+	set<string> resultModifies3 = pkb.getResultsOfRS(MODIFIES,
+		*(new StmtNum(2)), *(new Declaration(EntityType::VAR, "v")))[""];
+	string d3[] = {  "x", "y", "z", "t", "count" };
+	REQUIRE(set<string>{ std::begin(d3), std::end(d3) } == resultModifies3);
+
 }
