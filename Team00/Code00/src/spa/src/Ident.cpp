6095fe44e91a26bdc3905b5196af9c526cf99f51
Bruce Ong <bruceskellator@hotmail.com>
2021-02-16 17:19:59 +0800

    Removed common files and added query interface inside QueryParser

--- Team00/Code00/src/spa/src/Ident.cpp
+++
@@ -1,6 +0,0 @@
-#include "Ident.h"
-
-Ident::Ident(QueryInputType queryInputType, std::string value) {
-    m_queryInputType = queryInputType;
-    m_value = value;
-}
--- Team00/Code00/src/spa/src/QueryInput.cpp
+++
@@ -1,9 +0,0 @@
-#include "QueryInput.h"
-
-QueryInputType QueryInput::getType() {
-	return m_queryInputType;
-}
-
-std::string QueryInput::getValue() {
-	return m_value;
-}
