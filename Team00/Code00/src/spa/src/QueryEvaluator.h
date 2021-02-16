77d196a1eae5a353243d6af0d2de2356d6f4b6c0
davidcwh <dcwh12321@gmail.com>
2021-02-16 16:34:51 +0800

    Add QueryInterface

--- Team00/Code00/src/spa/src/QueryEvaluator.h
+++ Team00/Code00/src/spa/src/QueryEvaluator.h
@@ -3,7 +3,7 @@
 #include <set>
 #include <string>
 #include <memory>
-#include "Query.h"
+#include "QueryInterface.h"
 #include "QueryInputType.h"
 #include "PKBInterface.h"
 #include "ResultUtil.h"
@@ -11,21 +11,19 @@
 
 class QueryEvaluator {
 private:
-	shared_ptr<Query> aQuery;
+	shared_ptr<QueryInterface> aQuery;
 	shared_ptr<PKBInterface> aPKB;
 
 	shared_ptr<ResultsTable> evaluateRelationshipClauses(vector<shared_ptr<RelationshipClause>> relationshipClauses, shared_ptr<ResultsTable> results);
 
 	shared_ptr<ResultsTable> evaluatePatternClauses(vector<shared_ptr<PatternClause>> patternClauses, shared_ptr<ResultsTable> results);
 
-	vector<unordered_map<string, string>> evaluateSelectClause(shared_ptr<SelectClause> selectClause);
-
 	shared_ptr<ResultsTable> mergeResults(unordered_map <string, set<string>> PKBResults, vector<string> synonyms,
 		shared_ptr<ResultsTable> currentResults);
 	
 public:
 	
-	QueryEvaluator(shared_ptr<Query> query, shared_ptr<PKBInterface> pkb);
+	QueryEvaluator(shared_ptr<QueryInterface> query, shared_ptr<PKBInterface> pkb);
 
 	shared_ptr<ResultsTable> evaluate();
 };
