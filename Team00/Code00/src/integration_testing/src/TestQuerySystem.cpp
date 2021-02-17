23416d10b516c99cbe8713c87f9d67ad773f7fb9
davidcwh <dcwh12321@gmail.com>
2021-02-17 13:36:12 +0800

    Added Basic Query System Integration Tests

--- Team00/Code00/src/integration_testing/src/TestQuerySystem.cpp
+++ Team00/Code00/src/integration_testing/src/TestQuerySystem.cpp
@@ -41,7 +41,65 @@ TEST_CASE("First Test") {
         QueryEvaluator queryEvaluator = QueryEvaluator(query, pkb);
         auto evaluatedResults = queryEvaluator.evaluate();
         ResultsProjector::projectResults(evaluatedResults, query->getSelectClause(), pkb, results);
-        TestResultsTableUtil::checkList(results, { "13", "2", "5" });
+        TestResultsTableUtil::checkList(results, {"2", "13", "5" });
+
+    }
+
+    SECTION("Query with Select Clause + Pattern Clause") {
+        input = "assign a; variable x; Select x pattern a(x, _);";
+
+        auto query = std::make_shared<Query>();
+        QueryParser queryParser = QueryParser{ input, query };
+        queryParser.parse();
+
+        auto pkb = make_shared<PKBStub>();
+        std::unordered_map<std::string, std::set<std::string>> expectedMap = { {"2", {"x1", "count"}}, { "5", {"i", "x"} }, {"13", {"num8"} } };
+        pkb->setGetEntitiesReturnValue({ "henz", "count", "num8", "i", "david", "x", "x2", "x1" });
+        pkb->setGetResultsOfPatternReturnValue(expectedMap);
+        QueryEvaluator queryEvaluator = QueryEvaluator(query, pkb);
+        auto evaluatedResults = queryEvaluator.evaluate();
+        ResultsProjector::projectResults(evaluatedResults, query->getSelectClause(), pkb, results);
+        TestResultsTableUtil::checkList(results, { "count", "x1", "num8", "i", "x" });
+
+    }
+
+    SECTION("Query with Select Clause with related synonym + both Clauses") {
+        input = "assign a; variable x; stmt s; Select x such that Uses(s, x) pattern a(x, _);";
+
+        auto query = std::make_shared<Query>();
+        QueryParser queryParser = QueryParser{ input, query };
+        queryParser.parse();
+
+        auto pkb = make_shared<PKBStub>();
+        std::unordered_map<std::string, std::set<std::string>> expectedPatternMap = { {"2", {"x1", "count"}}, { "5", {"i", "x"} }, {"13", {"num8"} } };
+        std::unordered_map<std::string, std::set<std::string>> expectedRelationshipMap = { {"4", {"x1"}}, { "53", {"x", "martin"} }, {"5", {"bruce"}}, {"2", {"x2"}} };
+        pkb->setGetEntitiesReturnValue({ "henz", "count", "num8", "i", "x", "x2", "x1", "martin", "bruce", "tuan" });
+        pkb->setGetResultsOfPatternReturnValue(expectedPatternMap);
+        pkb->setGetResultsOfRSReturnValue(expectedRelationshipMap);
+        QueryEvaluator queryEvaluator = QueryEvaluator(query, pkb);
+        auto evaluatedResults = queryEvaluator.evaluate();
+        ResultsProjector::projectResults(evaluatedResults, query->getSelectClause(), pkb, results);
+        TestResultsTableUtil::checkList(results, { "x1", "x" });
+
+    }
+
+    SECTION("Query with Select Clause with unrelated synonym + both Clauses") {
+        input = "assign a; variable x; stmt s; Select s such that Uses(a, x) pattern a(x, _);";
+
+        auto query = std::make_shared<Query>();
+        QueryParser queryParser = QueryParser{ input, query };
+        queryParser.parse();
+
+        auto pkb = make_shared<PKBStub>();
+        std::unordered_map<std::string, std::set<std::string>> expectedPatternMap = { {"2", {"x1", "count"}}, { "5", {"i", "x"} }, {"13", {"num8"} } };
+        std::unordered_map<std::string, std::set<std::string>> expectedRelationshipMap = { {"4", {"x1"}}, { "15", {"x", "martin"} }, {"5", {"bruce"}}, {"2", {"x2"}} };
+        pkb->setGetEntitiesReturnValue({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" });
+        pkb->setGetResultsOfPatternReturnValue(expectedPatternMap);
+        pkb->setGetResultsOfRSReturnValue(expectedRelationshipMap);
+        QueryEvaluator queryEvaluator = QueryEvaluator(query, pkb);
+        auto evaluatedResults = queryEvaluator.evaluate();
+        ResultsProjector::projectResults(evaluatedResults, query->getSelectClause(), pkb, results);
+        TestResultsTableUtil::checkList(results, { "1", "10", "11", "12", "13", "14", "15", "2", "3", "4", "5", "6", "7", "8", "9" });
 
     }
 }
