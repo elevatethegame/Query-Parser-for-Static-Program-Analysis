70717750c8c52a3859b89e0452c38ba5ed934aeb
JunHongT <teojunhong96@gmail.com>
2021-02-20 01:26:13 +0800

    Edit TestWrapper

--- Team00/Code00/src/autotester/src/TestWrapper.cpp
+++ Team00/Code00/src/autotester/src/TestWrapper.cpp
@@ -3,6 +3,16 @@
 #include "ResultsProjector.h"
 #include "QueryInterface.h"
 #include "QueryEvaluator.h"
+#include <Parser.h>
+#include <Query.h>
+
+#include "catch.hpp"
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -17,20 +27,60 @@ volatile bool AbstractWrapper::GlobalStop = false;
 TestWrapper::TestWrapper() { 
   // create any objects here as instance variables of this class
   // as well as any initialization required for your spa program
-	
 }
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
 	// call your parser to do the parsing
-  // ...rest of your code...
+
+	vector<string> codes;
+	// Open the File
+	std::ifstream in(filename.c_str());
+	// Check if object is valid
+	if (!in) {
+		std::cerr << "Cannot open the File : " << filename << std::endl;
+	}
+
+	std::string str;
+	// Read the next line from File untill it reaches the end.
+	while (std::getline(in, str))
+	{
+		// Line contains string of length > 0 then save it in vector
+		if (str.size() > 0)
+			codes.push_back(str);
+	}
+	//Close The File
+	in.close();
+
+	SIMPLETokenStream stream{ codes };
+
+	SIMPLETokenStream secondStream{ stream };
+	// while (secondStream.isEmpty() == false) {
+	//     cerr << secondStream.getToken().toString() << endl;
+	// }
+
+	DesignExtractor extractor;
+	Parser parser{ extractor };
+
+	auto error = parser.parseProcedure(stream);
+	
+	shared_ptr<PKB> pkb = extractor.extractToPKB();
+
+	pkb->init();
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
-  // ...code to evaluate query...
+void TestWrapper::evaluate(std::string input, std::list<std::string>& results) {
+	// call your evaluator to evaluate the query here
+	  // ...code to evaluate query...
+	shared_ptr<PKB> pkb;
+	auto query = std::make_shared<Query>();
+	QueryParser queryParser = QueryParser{ input, query };
+	queryParser.parse();
+	// store the answers to the query in the results list (it is initially empty)
+	// each result must be a string.
+	QueryEvaluator queryEvaluator = QueryEvaluator(query, pkb);
+	auto evaluatedResults = queryEvaluator.evaluate();
+	ResultsProjector::projectResults(evaluatedResults, query->getSelectClause(), pkb, results);
 
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
 }
