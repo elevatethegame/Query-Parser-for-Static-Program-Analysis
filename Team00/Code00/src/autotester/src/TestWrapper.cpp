ea0fd593e8fdf8e452988d2b41d6b0df0cc62bd0
JunHongT <teojunhong96@gmail.com>
2021-02-20 16:40:33 +0800

    Fix TestWrapper

--- Team00/Code00/src/autotester/src/TestWrapper.cpp
+++ Team00/Code00/src/autotester/src/TestWrapper.cpp
@@ -7,7 +7,7 @@
 #include <Query.h>
 
 #include "catch.hpp"
-
+#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
@@ -29,10 +29,12 @@ TestWrapper::TestWrapper() {
   // as well as any initialization required for your spa program
 }
 
+shared_ptr<PKB> pkb;
+
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename) {
 	// call your parser to do the parsing
-
+	
 	vector<string> codes;
 	// Open the File
 	std::ifstream in(filename.c_str());
@@ -54,17 +56,12 @@ void TestWrapper::parse(std::string filename) {
 
 	SIMPLETokenStream stream{ codes };
 
-	SIMPLETokenStream secondStream{ stream };
-	// while (secondStream.isEmpty() == false) {
-	//     cerr << secondStream.getToken().toString() << endl;
-	// }
-
 	DesignExtractor extractor;
 	Parser parser{ extractor };
 
 	auto error = parser.parseProcedure(stream);
 	
-	shared_ptr<PKB> pkb = extractor.extractToPKB();
+	pkb = extractor.extractToPKB();
 
 	pkb->init();
 }
@@ -73,14 +70,35 @@ void TestWrapper::parse(std::string filename) {
 void TestWrapper::evaluate(std::string input, std::list<std::string>& results) {
 	// call your evaluator to evaluate the query here
 	  // ...code to evaluate query...
-	shared_ptr<PKB> pkb;
+	//cout << "Test evaluate method\n";
 	auto query = std::make_shared<Query>();
 	QueryParser queryParser = QueryParser{ input, query };
-	queryParser.parse();
+	try {
+		
+		
+		queryParser.parse();
+	}
+	catch (std::exception err) {
+		cout << err.what() << "\n";
+	}
+
+	
 	// store the answers to the query in the results list (it is initially empty)
 	// each result must be a string.
+	//cout << query->getSelectClause()->getDeclaration()->getValue() << "\n";
+	//cout << query->getRelationshipClauses().size() << "\n";
 	QueryEvaluator queryEvaluator = QueryEvaluator(query, pkb);
+	//set<string> r = pkb->getEntities(EntityType::STMT);
+	shared_ptr<QueryInput> stmtnum1 =
+		dynamic_pointer_cast<QueryInput>(make_shared<StmtNum>(1));
+	shared_ptr<QueryInput> stmtnum2 =
+		dynamic_pointer_cast<QueryInput>(make_shared<StmtNum>(2));
+	cout << "getBooleanResultOfRS: " << pkb->getBooleanResultOfRS(RelationshipType::FOLLOWS, stmtnum1, stmtnum2) << "\n";
+	shared_ptr<QueryInput> dec =
+		dynamic_pointer_cast<QueryInput>(make_shared<Declaration>(EntityType::STMT, "s"));
+	cout << "getResultsOfRS: " << pkb->getResultsOfRS(RelationshipType::FOLLOWS, dec, stmtnum2).size() << "\n";
 	auto evaluatedResults = queryEvaluator.evaluate();
+	//cout << evaluatedResults->isNoResult() << "\n";
 	ResultsProjector::projectResults(evaluatedResults, query->getSelectClause(), pkb, results);
 
 }
