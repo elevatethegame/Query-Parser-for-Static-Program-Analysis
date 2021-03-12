35301d7a03fc97be86991669f98a4d33607834ca
youaremysky99 <lequangtuan6b@gmail.com>
2021-03-13 03:28:59 +0800

    Update DesignExtractorHelper.h

--- Team00/Code00/src/spa/src/DesignExtractorHelper.h
+++ Team00/Code00/src/spa/src/DesignExtractorHelper.h
@@ -76,10 +76,57 @@ Ownership<K, V> extractOwnerships(
     return results;
 }
 
+template<typename A> 
+unordered_set<A> mergeSet(const unordered_set<A>& f, const unordered_set<A> &g) {
+    unordered_set<A> results;
+    for (auto &x: f) results.insert(x);
+    for (auto &x: g) results.insert(x);
+    return results;
+}
+
+template<typename A, typename B>
+Ownership<A, B> combine(const Ownership<A, B>& f, const Ownership<A, B> &g) {
+    Ownership<A, B> results;
+    unordered_set<A> allVertices = mergeSet<A>(extractVertices(f), extractVertices(g));
+
+    for (auto& u: allVertices) {
+        unordered_set<A> collecting;
+        if (f.find(u) != f.end()) {
+            for (auto&x : f.at(u)) {
+                collecting.insert(x);
+            }
+        }
+        if (g.find(u) != g.end()) {
+            for (auto&x : g.at(u)) {
+                collecting.insert(x);
+            }
+        }
+        copy(collecting.begin(), collecting.end(), std::back_inserter(results[u]));
+    }
+
+    return results;
+}
+
 
 template<typename A, typename B, typename C>
 Ownership<A, C> convolute(const Ownership<A, B> &f, const Ownership<B, C> &g) {
+    ///need to solve in |A||B| + |A||C| + |B||C|
+    Ownership<A, C> results;
 
+    unordered_set<A> allVertices = extractVertices(f);
+    for (auto& u: allVertices) {
+        unordered_set<C> collecting;
+        for (auto& v: f.at(u)) {
+            if (g.find(v) != g.end()) {
+                for (auto& w: g(v)) {
+                    collecting.insert(w);
+                }
+            }
+        }
+        copy(collecting.begin(), collecting.end(), std::back_inserter(results[u]));
+    }
+    
+    return results;
 }
 
 
