deb3d3753b540f1744beda3faa1a664019589aec
youaremysky99 <lequangtuan6b@gmail.com>
2021-03-15 09:09:34 +0800

    some-more-tests

--- Team00/Code00/src/unit_testing/src/TestDesignExtractorHelper.cpp
+++ Team00/Code00/src/unit_testing/src/TestDesignExtractorHelper.cpp
@@ -15,7 +15,7 @@ void normalize(Direct<T>& edges) {
 }
 
 template<typename K, typename V>
-void normalize(Ownership<K, V> edges) {
+void normalize(Ownership<K, V>& edges) {
     for (auto& x: edges) {
         sort(x.second.begin(), x.second.end());
     }
@@ -54,7 +54,7 @@ TEST_CASE("Extract stars simple") {
     //2 -> 4
     //3 -> 4
     auto stars = extractStars<int> (edges);
-    
+
     Indirect<int> answer;
     answer[1] = {2, 3, 4};
     answer[2] = {3, 4};
@@ -64,4 +64,103 @@ TEST_CASE("Extract stars simple") {
     normalize(stars);
 
     REQUIRE(compareRelationships(answer, stars));
+}
+
+TEST_CASE("Extract stars with cycles") {
+    Direct<int> edges = {
+        {
+            1,
+            {2, 3}
+        },
+        {
+            2,
+            {3, 4}
+        },
+        {
+            3,
+            {1}
+        }
+    };
+    //1 -> 2
+    //1 -> 3
+    //2 -> 3
+    //2 -> 4
+    //3 -> 1
+    auto stars = extractStars<int> (edges);
+
+    Indirect<int> answer;
+    answer[1] = {2, 3, 4, 1};
+    answer[2] = {3, 4, 1, 2};
+    answer[3] = {1, 2, 3, 4};
+
+    normalize(answer);
+    normalize(stars);
+
+    REQUIRE(compareRelationships(answer, stars));
+}
+
+TEST_CASE("Test extract ownership simple") {
+    Indirect<int> edges = {
+        {
+            1,
+            {2, 3}
+        },
+        {
+            2,
+            {4}
+        },
+        {
+            3,
+            {4}
+        },
+        {
+            4,
+            {}
+        }
+    };
+    //1 -> 2
+    //1 -> 3
+    //2 -> 4
+    //3 -> 4
+
+    Ownership<int, string> ownerships = {
+        {
+            1,
+            {"a", "b"}
+        },
+        {
+            2, 
+            {"c"}
+        },
+        {
+            3,
+            {"d"}
+        },
+        {
+            4, {"e"}
+        }
+    };
+
+    auto result = extractOwnerships(edges, ownerships);
+
+    Ownership<int, string> answer = {
+        {
+            1, {"a", "b", "c", "d"}
+        },
+        {
+            2, {"c", "e"}
+        },
+        {
+            3, {"d", "e"}
+        },
+        {
+            4, {"e"}
+        }
+    };
+
+
+    normalize(answer);
+    normalize(result);
+    
+    REQUIRE(compareRelationships(answer, result));
 }
