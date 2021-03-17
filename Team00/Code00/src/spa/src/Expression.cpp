6f5661fbf95f2456ad2a54e3d9a3305b30833cb3
davidcwh <dcwh12321@gmail.com>
2021-03-17 20:53:41 +0800

    Update Expression class

--- Team00/Code00/src/spa/src/Expression.cpp
+++ Team00/Code00/src/spa/src/Expression.cpp
@@ -3,14 +3,27 @@
 
 using namespace std;
 
-Expression::Expression(string value) : aValue(value) {}
+Expression::Expression(string value, ExpressionType t) : aValue(value), type(t) {}
+
+Expression::Expression(string value) : aValue(value) {} // remove after merge with QPP
+
+// remove after merge with PKB/Parser
+Expression Expression::combineExpression(string operand, const Expression& other) {
+	//cout << "combining " <<  "(" + this->getValue() + operand + other.getValue() + ")" << endl;
+	return Expression("(" + this->getValue() + operand + other.getValue() + ")");
+}
 
 //currently just return string addition
-Expression Expression::combineExpression(string operand, const Expression& other) {
-    //cout << "combining " <<  "(" + this->getValue() + operand + other.getValue() + ")" << endl;
-	return Expression( "(" + this->getValue() + operand + other.getValue() + ")" );
+Expression Expression::combineExpression(
+	string operand, const Expression& other, ExpressionType t) {
+	//cout << "combining " <<  "(" + this->getValue() + operand + other.getValue() + ")" << endl;
+	return Expression("(" + this->getValue() + operand + other.getValue() + ")", t);
 }
 
 string Expression::getValue() const {
 	return this->aValue;
+}
+
+ExpressionType Expression::getType() const {
+	return this->type;
 }
